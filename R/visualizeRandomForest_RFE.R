a15b90d79d7f580881c8319e1c19be70f2b3a202
Carolin Loos <carolin.loos@gmx.de>
2019-12-07 14:54:46 -0500

    renaming and bugfix

--- R/visualizeRandomForest_RFE.R
+++ R/visualizeRandomForest_RFE.R
@@ -5,22 +5,22 @@
 #' @export
 
 visualizeRandomForest_RFE <- function(X, y) {
-    
-    XRed <- X
+
+    X_red <- X
     featSel <- data.frame(matrix(1, ncol = dim(X)[2], nrow = dim(X)[2] + 1))
     rownames(featSel) <- c(colnames(X), "oob")
     colnames(featSel) <- 1:dim(X)[2]
-    
+
     for (ind in 1:(dim(X)[2] - 1)) {
-        rf <- randomForest(as.matrix(XRed), as.factor(y))
+        rf <- randomForest(as.matrix(X_red), as.factor(y))
         featSel["oob", ind] <- mean(predict(rf) != y)
         remFeature <- rownames(rf$importance)[which(rf$importance == min(rf$importance))]
-        XRed <- XRed[, -which(colnames(XRed) == remFeature)]
+        XRed <- X_red[, -which(colnames(X_red) == remFeature)]
         featSel[remFeature, ind:dim(X)[2]] <- 0
     }
     featSel["oob", dim(X)[2]] <- NA
-    
-    dfPlot <- data.frame(oob = t(featSel["oob", ]), iteration = c(1:80))
+
+    dfPlot <- data.frame(oob = t(featSel["oob", ]), iteration = c(1:dim(X)[2]))
     p <- ggplot(dfPlot, aes(iteration, oob)) + geom_point() + geom_line()
     plot(p)
 }
