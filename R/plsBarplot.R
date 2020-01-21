72b6f2c1c016efe1deab208f45882e2606f607a1
Carolin Loos <carolin.loos@gmx.de>
2020-01-21 10:29:54 -0500

    Update plsBarplot.R
    allow to filter for VIP scores > 1 in plotting

--- R/plsBarplot.R
+++ R/plsBarplot.R
@@ -1,7 +1,8 @@
 #' @export
 
 plsBarplot <- function(oplsda, y, X_sel, saveFlag = FALSE, fileStr = "",
-                       color1 = "red", color2 = "blue", feature_annot = data.frame()) {
+                       color1 = "red", color2 = "blue", feature_annot = data.frame(),
+                       width = 5, height = 4, vipFlag = FALSE) {
 
     if (dim(feature_annot)[1] == 0) {
         feature_annot <- data.frame(useColor = rep("black", dim(X_sel)[2]),
@@ -27,18 +28,25 @@ plsBarplot <- function(oplsda, y, X_sel, saveFlag = FALSE, fileStr = "",
                         features = colnames(X_sel))
     colnames(dfBar) <- c("vipScores", "loadingsLV1", "features")
 
+
     # check here whether the order is right!
     dfBar$mark[dfBar$loadingsLV1 < 0] = "<0"
     dfBar$mark[dfBar$loadingsLV1 > 0] = ">0"
+    dfBar$mark <- factor(dfBar$mark, levels = c("<0", ">0"))
 
     dfBar <- dfBar[order(dfBar$vipScores), ]
     dfBar$features <- factor(dfBar$features, levels = unique(dfBar$features))
 
+    # only VIP > 1
+    if (vipFlag) {
+        dfBar <- dfBar[dfBar$vipScores > 1, ]
+    }
     # plot loadings sorted according to the VIP score and color coding it
     # according to enrichent in classes
     pltBar <- ggplot(data = dfBar, aes(x = features, y = loadingsLV1, fill = mark)) +
         geom_bar(stat = "identity", color = "black") +
-        scale_fill_manual(breaks = unique(dfBar$mark), values = c(color1, color2)) +
+        scale_fill_manual(values = c("<0" = color1,
+                                     ">0" = color2), breaks = c("<0", ">0")) +
         theme_minimal() +
         coord_flip() +
         xlab("") +
@@ -48,7 +56,7 @@ plsBarplot <- function(oplsda, y, X_sel, saveFlag = FALSE, fileStr = "",
                                                                                rownames(feature_annot))]))
 
     if (saveFlag) {
-        pdf(paste(fileStr, "barPlot.pdf", sep = "_"), width = 5, height = 4)
+        pdf(paste(fileStr, "barPlot.pdf", sep = "_"), width = width, height = height)
         print(pltBar)
         dev.off()
     }
@@ -57,17 +65,21 @@ plsBarplot <- function(oplsda, y, X_sel, saveFlag = FALSE, fileStr = "",
     # plot VIP scores and color coding it according to enrichent in classes
     pltVip <- ggplot(data = dfBar, aes(x = features, y = vipScores, fill = mark)) +
         geom_bar(stat = "identity", color = "black") +
-        scale_fill_manual(breaks = unique(dfBar$mark), values = c(color1, color2)) +
+        scale_fill_manual(values = c("<0" = color1,
+                                     ">0" = color2), breaks = c("<0", ">0")) +
         theme_minimal() + coord_flip() + xlab("") +
         ylab("VIP scores") +
         theme(legend.position = "none",
               axis.text.y = element_text(colour = feature_annot$useColor[match(dfBar$features[order(dfBar$vipScores)],
                                                                                rownames(feature_annot))]))
     if (saveFlag) {
-        pdf(paste(fileStr, "vipPlot.pdf", sep = "_"), width = 5, height = 4)
+        pdf(paste(fileStr, "vipPlot.pdf", sep = "_"), width = width, height = height)
         print(pltVip)
         dev.off()
     }
     print(pltVip)
+
+    output <- list(pltBar = pltBar, pltVip = pltVip)
+    return(output)
 
 }
