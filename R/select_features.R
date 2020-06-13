e89ac0b7c77247b046fedaa0b28535c8b30e0d96
Carolin Loos <carolin.loos@gmx.de>
2020-06-13 15:21:17 -0400

    first feature selection

--- R/select_features.R
+++ R/select_features.R
@@ -1,3 +1,9 @@
-select_features <- function(X, y, options) {
-  return("Doeh")
+select_features <- function(X, y, options = FALSE) {
+
+  result_lasso <- cv.glmnet(X, y, type.measure = "mse", alpha = 1,
+                        family = "binomial", nfolds = 5)
+  coefs <- coef(result_lasso, s = "lambda.min")
+  selected_features <-  rownames(coefs)[which(!(coefs == 0))[-1]]
+
+  return(selected_features)
 }
