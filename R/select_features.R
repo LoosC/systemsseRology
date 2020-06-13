bc999c4caf42cab49ff5d76d9753688635a3fbc4
Carolin Loos <carolin.loos@gmx.de>
2020-06-13 16:19:00 -0400

    Update select_features.R

--- R/select_features.R
+++ R/select_features.R
@@ -13,6 +13,7 @@ select_features <- function(X, y, options = FALSE) {
   result_lasso <- glmnet::cv.glmnet(X, y, type.measure = "mse", alpha = 1,
                                     family = "binomial", nfolds = 5)
   coefs <- coef(result_lasso, s = "lambda.min")
+  # first entry of coefs is the intercept, exclude it from the search
   sel_features <- rownames(coefs)[which(!(coefs[-1,1] == 0)) + 1]
   return(sel_features)
 }
