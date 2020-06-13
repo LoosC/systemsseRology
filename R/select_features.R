a6e36092d8e75a111d4de828ec9ae15e90bc1ea3
Carolin Loos <carolin.loos@gmx.de>
2020-06-13 15:57:04 -0400

    update

--- R/select_features.R
+++ R/select_features.R
@@ -1,7 +1,17 @@
+#' Feature selection
+#'
+#' @param X
+#' @param y
+#' @param options
+#'
+#' @return
+#' @export
+#'
+#' @examples
 select_features <- function(X, y, options = FALSE) {
 
-  result_lasso <- cv.glmnet(X, y, type.measure = "mse", alpha = 1,
-                        family = "binomial", nfolds = 5)
+  result_lasso <- glmnet::cv.glmnet(X, y, type.measure = "mse", alpha = 1,
+                                    family = "binomial", nfolds = 5)
   coefs <- coef(result_lasso, s = "lambda.min")
   sel_features <- rownames(coefs)[which(!(coefs[,1] == 0))[-1]]
   return(sel_features)
