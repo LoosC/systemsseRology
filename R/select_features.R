277fce530b5783488d11dc9ce5d2cf262b0659bd
Carolin Loos <carolin.loos@gmx.de>
2020-06-13 16:18:09 -0400

    Update select_features.R

--- R/select_features.R
+++ R/select_features.R
@@ -13,6 +13,6 @@ select_features <- function(X, y, options = FALSE) {
   result_lasso <- glmnet::cv.glmnet(X, y, type.measure = "mse", alpha = 1,
                                     family = "binomial", nfolds = 5)
   coefs <- coef(result_lasso, s = "lambda.min")
-  sel_features <- rownames(coefs)[which(!(coefs[,1] == 0))[-1]]
+  sel_features <- rownames(coefs)[which(!(coefs[-1,1] == 0)) + 1]
   return(sel_features)
 }
