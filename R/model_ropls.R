aa03a7a7a4ab3bc8e49df9aa9d51cd45b1f0f59f
PSoosten <psgithub@protonmail.com>
2020-06-23 19:53:50 -0400

    suppress ropls errors

--- R/model_ropls.R
+++ R/model_ropls.R
@@ -8,12 +8,14 @@
 #'
 #' @examples
 train_ropls <- function(X, y) {
+  sink(file=tempfile(), type="output")
   try_out <- try(
     model <- ropls::opls(X, y,
                          crossValI = 5, # TODO make this an option
                          permI = 0, # no permutation and other output to save computation time
                          info.txtC = "none",
-                         fig.pdfC = "none")
+                         fig.pdfC = "none"),
+    silent = TRUE
   )
   if (is(try_out, "try-error")) {
     # No model could be build, provide a model with one latent variable (even if this is not significant)
@@ -23,6 +25,7 @@ train_ropls <- function(X, y) {
                          info.txtC = "none",
                          fig.pdfC = "none")
   }
+  sink()
   return(model)
 }
 
@@ -36,7 +39,6 @@ train_ropls <- function(X, y) {
 #'
 #' @examples
 predict_ropls <- function(model, X) {
-
   y_pred <- ropls::predict(model, newdata = X)
   return(y_pred)
 }
@@ -51,21 +53,31 @@ predict_ropls <- function(model, X) {
 #'
 #' @examples
 pca_ropls <- function(X) {
-  try_out <- try(
-    model <- ropls::opls(X, predI = NA,
-                         crossValI = 5, # TODO make this an option
-                         permI = 0, # no permutation and other output to save computation time
-                         info.txtC = "none",
-                         fig.pdfC = "none")
-  )
-  if (is(try_out, "try-error") | ropls::getSummaryDF(model)$pre < 2) {
-    # to ensure that the model has at least two prinicipal components
-    model <- ropls::opls(X, predI = 2,
-                         crossValI = 5,
-                         permI = 0,
-                         info.txtC = "none",
-                         fig.pdfC = "none")
-  }
+    try_out <- try(
+      model <- ropls::opls(X, predI = NA,
+                           crossValI = 5, # TODO make this an option
+                           permI = 0, # no permutation and other output to save computation time
+                           info.txtC = "none",
+                           fig.pdfC = "none"
+                           )
 
+    )
+
+    # try_out <- tryCatch(model <- ropls::opls(X, predI = NA,
+    #                               crossValI = 5, # TODO make this an option
+    #                               permI = 0, # no permutation and other output to save computation time
+    #                               info.txtC = "none",
+    #                               fig.pdfC = "none"),
+    #          error = function(e){}, warning = function(w) {},  abort = function() {})
+
+    if (is(try_out, "try-error") | ropls::getSummaryDF(model)$pre < 2) {
+      # to ensure that the model has at least two prinicipal components
+      model <- ropls::opls(X, predI = 2,
+                           crossValI = 5,
+                           permI = 0,
+                           info.txtC = "none",
+                           fig.pdfC = "none"
+                           )
+    }
   return(model)
 }
