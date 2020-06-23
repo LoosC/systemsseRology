609f75950ca03a72f688f6d886e6207146438761
PSoosten <psgithub@protonmail.com>
2020-06-22 20:47:50 -0400

    select_repeat()

--- R/select_repeat.R
+++ R/select_repeat.R
@@ -12,14 +12,19 @@
 #' @examples
 select_repeat <- function(X, y, selector, trials = 100, threshold = 0.8) {
 
-  feature_count <- matrix(0L, nrow = 1, ncol = dim(X)[2])
-  colnames(feature_count) <- colnames(X)
+  feature_count <- rep(0, dim(X)[2])
+  names(feature_count) <- colnames(X)
 
   for (trial in 1:trials) {
     features <- selector(X, y)
-    feature_count[1, features] <- 1 + feature_count[1, features]
+    feature_count[features] <- 1 + feature_count[features]
   }
 
-  above_threshold <- feature_count[1, which(feature_count[1,] > threshold * trials)]
-  return(colnames(above_threshold))
+  selected <- feature_count[-which(feature_count <= threshold * trials)]
+
+  if (length(selected) == 0) {
+    return(NULL)
+  } else {
+    return(names(selected))
+  }
 }
