2727add744caaee7e13aedb89f15238248dc30cd
PSoosten <psgithub@protonmail.com>
2020-06-23 20:16:54 -0400

    Update model_ropls.R

--- R/model_ropls.R
+++ R/model_ropls.R
@@ -8,23 +8,26 @@
 #'
 #' @examples
 train_ropls <- function(X, y) {
+  # suppress annoying "error"s from ropls
   sink(file=tempfile(), type="output")
-  try_out <- try(
-    model <- ropls::opls(X, y,
+  try_out <- try(model <- ropls::opls(X, y,
                          crossValI = 5, # TODO make this an option
                          permI = 0, # no permutation and other output to save computation time
                          info.txtC = "none",
                          fig.pdfC = "none"),
-    silent = TRUE
+    silent = TRUE  # also need this to suppress errors
   )
   if (is(try_out, "try-error")) {
-    # No model could be build, provide a model with one latent variable (even if this is not significant)
+    # No model could be build, provide a model with one latent variable
+    # (even if this is not significant)
     model <- ropls::opls(X, y, predI = 1,
                          crossValI = 5,
                          permI = 0,
                          info.txtC = "none",
                          fig.pdfC = "none")
   }
+
+  # print error messages again
   sink()
   return(model)
 }
@@ -62,14 +65,6 @@ pca_ropls <- function(X) {
                            )
 
     )
-
-    # try_out <- tryCatch(model <- ropls::opls(X, predI = NA,
-    #                               crossValI = 5, # TODO make this an option
-    #                               permI = 0, # no permutation and other output to save computation time
-    #                               info.txtC = "none",
-    #                               fig.pdfC = "none"),
-    #          error = function(e){}, warning = function(w) {},  abort = function() {})
-
     if (is(try_out, "try-error") | ropls::getSummaryDF(model)$pre < 2) {
       # to ensure that the model has at least two prinicipal components
       model <- ropls::opls(X, predI = 2,
