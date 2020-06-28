b9602389769743a262ba9c5f5ec8ab611c67f9fd
PSoosten <psgithub@protonmail.com>
2020-06-28 11:29:25 -0400

    validate_repeat

---
+++ R/validate_repeat.R
@@ -0,0 +1,27 @@
+#' Title
+#'
+#' @param X
+#' @param y
+#' @param method
+#' @param options
+#' @param n_trials
+#'
+#' @return
+#' @export
+#'
+#' @examples
+validate_repeat <- function(X, y, method, options, n_trials=100) {
+  # we run validate() n_trials times, returning the vector of real scores
+  # and the matrices of random feature scores/permutation test scores
+
+  return_vals <- list()
+
+  for (trial in 1:n_trials) {
+    val_scores <- validate(X, y, method, options)
+    # remove the actual prediction from the validation
+    val_scores["cv_y"] <- NULL
+    return_vals[[trial]] <- val_scores
+  }
+
+  return(return_vals)
+}
