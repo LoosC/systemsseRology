d0823f23a1e9c7696fb1b93b18d704bdad44e466
Carolin Loos <carolin.loos@gmx.de>
2021-01-27 09:13:54 -0500

    Update pval_to_asteriks.R

--- R/pval_to_asteriks.R
+++ R/pval_to_asteriks.R
@@ -8,14 +8,16 @@
 pval_to_asteriks <- function(pval) {
   ret <- rep("", length = length(pval))
   for (ind in 1:length(pval)) {
-    if (pval[ind] < 0.05) {
-      ret[ind] <- "*"
-    }
-    if (pval[ind] < 0.01) {
-      ret[ind] <- "**"
-    }
-    if (pval[ind] < 0.001) {
-      ret[ind] <- "***"
+    if (!is.na(pval[ind])) {
+      if (pval[ind] < 0.05) {
+        ret[ind] <- "*"
+      }
+      if (pval[ind] < 0.01) {
+        ret[ind] <- "**"
+      }
+      if (pval[ind] < 0.001) {
+        ret[ind] <- "***"
+      }
     }
   }
   return(ret)
